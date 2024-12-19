3dc241b0928935f65ffaad1b7d91f27acb2e2d9c
Thi <anhthi157@gmail.com>
2024-12-19 10:36:20 +0700

    Commit thanh cong

---
+++ OrangeHRM/OrangeHRM/Pages/BasePage.cs
@@ -0,0 +1,13 @@
+﻿using OpenQA.Selenium;
+
+namespace OrangeHRM.Pages
+{
+    public class BasePage
+    {
+        protected IWebDriver driver;
+        public BasePage(IWebDriver _driver) 
+        { 
+            driver = _driver;
+        }
+    }
+}
