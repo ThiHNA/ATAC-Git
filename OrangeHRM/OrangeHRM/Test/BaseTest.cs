5403f7092be8820669c69cf99adfccde7d15f84a
Thi <anhthi157@gmail.com>
2024-12-19 10:38:22 +0700

    Commit thanh cong

---
+++ OrangeHRM/OrangeHRM/Test/BaseTest.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace OrangeHRM.Test
+{
+    [TestClass]
+    public class BaseTest
+    {
+        protected static IWebDriver driver;
+
+        [ClassInitialize(InheritanceBehavior.BeforeEachDerivedClass)]
+        public static void SetupBrowser(TestContext testContext)
+        {
+            // Open Chrome driver
+            driver = new ChromeDriver();
+
+            // Set implicit wait
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+        }
+
+        [ClassCleanup(InheritanceBehavior.BeforeEachDerivedClass)]
+        public static void CleanupBrowser()
+        {
+            driver.Quit();
+        }
+    }
+}
