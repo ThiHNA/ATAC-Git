98908e5b7d9cdf6a93f6bc46393890cf197f70a8
Thi <anhthi157@gmail.com>
2024-12-29 22:21:20 +0700

    Commit API Test

---
+++ OrangeHRM/APITest/Test/BaseTest.cs
@@ -0,0 +1,16 @@
+﻿using Automation.Core.Helpers;
+using RestSharp;
+
+namespace APITest.Test
+{
+    [TestClass]
+    public class BaseTest
+    {
+        protected RestClient client;
+        [TestInitialize]
+        public void SetupRestClient()
+        {
+            client = new RestClient(ConfigurationHelper.GetValue<string>("url"));
+        }
+    }
+}
---
+++ OrangeHRM/APITest/Test/UsersTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Net;
+using APITest.Model;
+using FluentAssertions;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace APITest.Test
+{
+    [TestClass]
+    public class UsersTest : BaseTest
+    {
+        [TestMethod]
+        public void Verify_Get_List_Users_By_Page()
+        {
+            var randomPage = new Random().Next(1, 2 + 1);
+            var request = new RestRequest($"/api/users?page={randomPage}", Method.Get);
+            RestResponse response = client.Execute(request);
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            var responseData = JsonConvert.DeserializeObject<ListUserModel>(response.Content);
+            responseData.page.Should().Be(randomPage);
+            responseData.data.Should().HaveCountGreaterThan(0);
+        }
+
+        [TestMethod]
+        public void Verify_Create_User()
+        {
+            var requestBody = new CreateUserRequestModel();
+            requestBody.name = "Thi";
+            requestBody.job = "Member";
+
+            var request = new RestRequest("/api/users", Method.Post);
+            request.AddJsonBody(requestBody);
+
+            RestResponse response = client.Execute(request);
+
+            response.StatusCode.Should().Be(HttpStatusCode.Created);
+
+            var responseData = JsonConvert.DeserializeObject<CreateUserReponseModel>(response.Content);
+            responseData.name.Should().Be(requestBody.name);
+            responseData.job.Should().Be(requestBody.job);
+
+        }
+    }
+}
